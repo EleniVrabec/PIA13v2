9a1da756d13cf283f5471cee8084429bfdcb47c6
EleniVrabec <eleni.vrabec@gritacademy.se>
2024-10-02 12:07:50 +0200

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import { Button, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+export default function Index() {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(prevCount => prevCount + 1);
+  const decrement = () => setCount(prevCount => (prevCount > 0 ? prevCount - 1 : 0 ))
+  return (
+    <SafeAreaView style={styles.safeArea}>
+    <View
+      style={styles.container}
+    >
+      <View style={styles.numberContainer}>
+           <Text style={styles.number}>{count}</Text>
+      </View>
+      
+
+      <View style={styles.btncontainer}>
+
+      <TouchableOpacity 
+      onPress={increment}
+      style={styles.button}
+      >
+           <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+      onPress={decrement}
+      style={styles.button}
+      >
+           <Text style={styles.buttonText}>-</Text>
+      </TouchableOpacity>
+
+      </View>
+      
+    </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1, 
+  },
+  container:{
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: 10,
+  },
+  numberContainer: {
+    flex: 2, 
+    justifyContent: "center", 
+    alignItems: "center", 
+  },
+  number:{
+    fontWeight: 'bold',
+    fontSize:80
+  },
+  btncontainer:{  
+    flex:0,
+    flexDirection:'row',
+    justifyContent: "center",
+    alignItems: "flex-end",
+    width: '80%',
+  },
+  button:{
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 20,
+    margin: 5,
+    borderRadius: 5,
+    width:100
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  }
+
+  }
+)
+
+
